bbfcde47b0bc0241ff152dc1cb7cb78c5ce14394
dashdashtomscratch <dashdashtomscratch@gmail.com>
2024-10-29 23:13:28 +0000

    some extremely basic code to get it working again

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-// Add JS here
+function toggleDarkMode() {
+    const body = document.body;
+    const bubuhImage = document.getElementById('bubuh');
+  
+    if (body.classList.contains('dark-mode')) {
+      body.classList.remove('dark-mode');
+      bubuhImage.src = 'bubuh.png';
+    } else {
+      body.classList.add('dark-mode');
+      bubuhImage.src = 'bubuh-dark.png';
+    }
+  }
